66071b68d76645e62a9f55904de52a7411e402a7
satyam281 <satyampandey281@gmail.com>
2023-09-18 23:51:13 +0530

    Update block_animation_script.js

--- block_animation_script.js
+++ block_animation_script.js
@@ -8,8 +8,8 @@ function adding_event_listeners(){
                 blocks_for_animation[i].addEventListener('mouseenter',() =>{
                     gsap.to(blocks_for_animation[i],{height:"40vh",minWidth:"32vh",width:"32vh",duration:0.1})
                     console.log("on")
-                    var audio = new Audio("sound.mp3");
-                    audio.play();
+                    // var audio = new Audio("sound.mp3");
+                    // audio.play();
                 })
                 blocks_for_animation[i].addEventListener('mouseleave',() =>{    
                     gsap.to(blocks_for_animation[i],{height:"20vh",minWidth:"20vh",width:"20vh",duration: 0.1})
